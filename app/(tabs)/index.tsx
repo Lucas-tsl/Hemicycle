5575f2f3e94a2471d22220b5a3fba4e776ff27c2
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-03-15 11:49:48 +0100

    Update dependencies and add missing imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,8 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>kjs;,bdc</Text>
+      <Text style={styles.title}>Scan The ID</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
